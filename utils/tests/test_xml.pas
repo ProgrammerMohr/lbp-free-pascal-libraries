877b58a80bf88a8d6a5c731be042bd4376b5b058
Lloyd Park <lloyd.b.park@gmail.com>
2019-01-31 22:14:13 -0500

    I forgot to commit the test code.

---
+++ utils/tests/test_xml.pas
@@ -0,0 +1,61 @@
+{* ***************************************************************************
+
+    Copyright (c) 2017 by Lloyd B. Park
+
+    test_trees - Test my generic trees
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or 
+    (at your option) any later version.
+
+
+    This program is distributed in the hope that it will be useful,but 
+    WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with this program.  If not, see 
+    <http://www.gnu.org/licenses/>.
+
+*************************************************************************** *}
+
+program test_trees;
+
+{$include lbp_standard_modes.inc}
+{$LONGSTRINGS ON}    // Non-sized Strings are ANSI strings
+
+uses
+//   lbp_generic_containers,
+   lbp_types,
+   lbp_xml,
+   lbp_generic_containers;
+
+
+// ************************************************************************
+// * TestAttribute()
+// ************************************************************************
+
+procedure TestAttribute();
+   var
+      A: tXmlAttribute;
+   begin
+      A:= tXmlAttribute.Create;
+      A.Add( 'href', 'http://www.w3.org/XML');
+      A.Add( 'Lloyd', 'Family');
+
+      writeln( '------- Testing XML Attribute functionality --------');
+      if( A.Find( 'href')) then writeln( '   Found: ', A.Value);
+
+      A.Destroy;
+   end; // TestAttribute()
+
+
+// ************************************************************************
+// * Main()
+// ************************************************************************
+
+begin
+   TestAttribute();
+end.  // test_xml program
