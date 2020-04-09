6a60b9c64851328b48c304186c61e3cb065e3b96
Lloyd B. Park <lloyd.b.park@gmail.com>
2020-04-09 19:11:38 -0400

    A tiny bit of progress

--- apps/csv/csv_sort.pas
+++ apps/csv/csv_sort.pas
@@ -12,15 +12,34 @@ uses
    lbp_output_file;
 
 
+// ************************************************************************
+// * Dictionary types
+// ************************************************************************
+
+type
+   tStringSortDictionary = specialize tgDictionary< string, tCsvStringArray>;
+   tInt64SortDictionary  = specialize tgDictionary< Int64, tCsvStringArray>;
+
+
+// ************************************************************************
+// * Global variables
+// ************************************************************************
+
 var
+   FieldName:          string; // From the --header parameter
+   FieldIndex:         integer;
    SkipNonPrintable:   boolean;
    IgnoreCase:         boolean;
    ReverseOrder:       boolean;
+   DelimiterIn:        char;
    DelimiterOut:       char;
    Csv:                tCsv;
    SortByIpv4:         boolean;
    SortByInteger:      boolean;
-   SortByDouble:       boolean;   
+   SortByDouble:       boolean;
+   StringDict:         tStringSortDictionary;
+   Int64Dict:          tInt64SortDictionary;
+
 
 
 // ************************************************************************
@@ -29,20 +48,8 @@ var
 
 procedure SetGlobals();
    var
-      L:            integer = 0;  // used for Length
-      Field:        string;
-      GrepFields:   tCsvStringArray;
       Delimiter:    string;
-      DelimiterIn:  char;
    begin
-      // Get the regular expression
-      if( Length( UnnamedParams) <> 1) then begin
-         raise tCsvException.Create( 'You must enter one and only one regular expression on the command line!');
-      end;
-      RegularExpression:= tRegExpr.Create( UnnamedParams[ 0]);
-      
-      GrepIndexes:= tIntegerList.Create();
-
       // Set the input delimiter
       if( ParamSet( 'id')) then begin
          Delimiter:= GetParam( 'id');
@@ -65,47 +72,39 @@ procedure SetGlobals();
          DelimiterOut:= DelimiterIn;
       end;
    
-      // Get the new header from the command line.
-      if( ParamSet( 'header')) then begin
-         Csv:= tCsv.Create( GetParam( 'header'));
-         Csv.Delimiter:= ','; // The delimiter for the command line is always a ','
-         Csv.SkipNonPrintable:= ParamSet( 's');
-         GrepFields:= Csv.ParseLine;
-         Csv.Destroy;
-         L:= Length( GrepFields);
-      end;
-
       // Open input CSV
       Csv:= tCsv.Create( lbp_input_file.InputStream, False);
-   
-      // If we don't yet have GrepHeaders, default to searchin all headers
       Csv.ParseHeader();
-      if( L = 0) then begin
-         GrepFields:= Csv.Header();
+   
+      // Get the header field to be sorted.
+      if( ParamSet( 'header')) then begin
+         FieldName:= GetParam( 'header');
+      end else begin
+         Usage( true, 'The header field parameter is required!');        
       end;
 
-      // Make sure all the header fields are valid and add their indexes to GrepIndexes
-      for Field in GrepFields do begin
-         if( Csv.ColumnExists( Field)) then begin
-            GrepIndexes.Queue := Csv.IndexOf( Field);
-         end else begin
-            Usage( true, 'Your header field ''' + Field + ''' does not exist in the input CSV file!');
-         end;
-      end; // for
+      // Make sure the header field is valid.
+      if( Csv.ColumnExists( FieldName)) then begin
+         FieldIndex := Csv.IndexOf( FieldName);
+      end else begin
+         Usage( true, 'Your header field ''' + FieldName + ''' does not exist in the input CSV file!');
+      end;
 
       // Set the boolean options
       SkipNonPrintable:= ParamSet( 'skip-non-printable');
       IgnoreCase:=       ParamSet( 'ignore-case');
-      ReverseOrder:=     ParamSet( '');
-      SortByIpv4:=       ParamSet( '');
-      SortByInteger:=    ParamSet( '');
-      :=      ParamSet( '');
+      ReverseOrder:=     ParamSet( 'reverse-order');
+      SortByIpv4:=       ParamSet( 'ipv4');
+      SortByInteger:=    ParamSet( 'number');
+      SortByDouble:=     ParamSet( 'double');
 
       // Apply the boolean options
       Csv.SkipNonPrintable:= SkipNonPrintable;
-      RegularExpression.ModifierI:= IgnoreCase;
-      RegularExpression.ModifierM:= true; // start and end line works for each line in a multi-line field
-   end; // InitGlobals()
+
+      // Initialize the dictionaries;
+      StringDict:= tStringSortDictionary.Create( tStringSortDictionary.tCompareFunction(@CompareString), true);
+      Int64Dict:=  tInt64SortDictionary.Create( tInt64SortDictionary.tCompareFunction(@CompareInt64), true);
+   end; // SetGlobals()
 
 
 // ************************************************************************
@@ -132,7 +131,7 @@ procedure InitArgvParser();
       InsertParam( ['i', 'ignore-case'], false, '', 'Perform a case insensitive sort.');
       InsertParam( ['4', 'ipv4'], false, '', 'The passed field is an IPv4 address.');
       InsertParam( ['n', 'number'], false, '', 'The passed filed is an integer number.');
-      InsertParam( ['d', 'double', 'float'], false, '', 'The passed filed is an floating point number.');
+      InsertParam( ['double', 'float'], false, '', 'The passed filed is an floating point number.');
       InsertParam( ['r', 'reverse-order'], false, '', 'Outputs in decending order.');
       InsertUsage();
       ParseParams();
@@ -140,11 +139,25 @@ procedure InitArgvParser();
 
 
 // ************************************************************************
+// * CompareStrings() - compares two strings. Used by the string dictionary
+// ************************************************************************
+
+
+// ************************************************************************
+// * CompareInt64() - Compare two Int64s.  Used by the Int64 dictionary
+// ************************************************************************
+
+
+// ************************************************************************
+// * 
+// ************************************************************************
+
+
+// ************************************************************************
 // * main()
 // ************************************************************************
 
 var
-   Csv:       tCsv;
    Header:    tCsvStringArray;
    TempLine:  tCsvStringArray;
    NewLine:   tCsvStringArray;
@@ -157,6 +170,7 @@ var
    iMax:      integer; 
 begin
    InitArgvParser();
+   SetGlobals();
 
    // // Set the input delimiter
    // if( ParamSet( 'id')) then begin
@@ -208,5 +222,9 @@ begin
    //    end;
    // until( C = EOFchr);
 
-   // Csv.Destroy;
+   StringDict.RemoveAll( True);
+   StringDict.Destroy();
+   Int64Dict.RemoveAll( True);
+   Int64Dict.Destroy();
+   Csv.Destroy;
 end.  // csv_sort program
