51950b4d856dc48aa997590b6d940951bcdb50ea
Lloyd B. Park <lloyd.b.park@gmail.com>
2020-04-10 23:21:42 -0400

    Some progress on csv_sort

--- apps/csv/csv_sort.pas
+++ apps/csv/csv_sort.pas
@@ -1,3 +1,40 @@
+{* ***************************************************************************
+
+Copyright (c) 2019 by Lloyd B. Park
+
+Sorts a .csv file according to the values in one field
+
+This file is part of Lloyd's Free Pascal Libraries (LFPL).
+
+    LFPL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 2.1 of the 
+    License, or (at your option) any later version with the following 
+    modification:
+
+    As a special exception, the copyright holders of this library 
+    give you permission to link this library with independent modules
+    to produce an executable, regardless of the license terms of these
+    independent modules, and to copy and distribute the resulting 
+    executable under terms of your choice, provided that you also meet,
+    for each linked independent module, the terms and conditions of 
+    the license of that module. An independent module is a module which
+    is not derived from or based on this library. If you modify this
+    library, you may extend this exception to your version of the 
+    library, but you are not obligated to do so. If you do not wish to
+    do so, delete this exception statement from your version.
+
+    LFPL is distributed in the hope that it will be useful,but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General 
+    Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with LFPL.  If not, see <http://www.gnu.org/licenses/>.
+
+*************************************************************************** *}
+
+
 program csv_sort;
 
 {$include lbp_standard_modes.inc}
@@ -9,16 +46,69 @@ uses
    lbp_parse_helper,
    lbp_generic_containers,
    lbp_input_file,
-   lbp_output_file;
+   lbp_output_file,
+   sysutils; // Conversion functions
+
 
 
 // ************************************************************************
-// * Dictionary types
+// * tCsvLine class - A class to hold the elements of a tCsvStringArrayraise 
+// *   We use this because LBP generic contains don't work with arrays.
 // ************************************************************************
 
 type
-   tStringSortDictionary = specialize tgDictionary< string, tCsvStringArray>;
-   tInt64SortDictionary  = specialize tgDictionary< Int64, tCsvStringArray>;
+   tCsvLine = class
+      public
+         Row:  tCsvStringArray;
+      end;
+      
+
+// ************************************************************************
+// * tCsvLineList class - A parent class to hold all CSV lines with the
+// *    same value in the sort field.
+// ************************************************************************
+
+type
+   tCsvLineList = specialize tgDoubleLinkedList< tCsvLine>;
+
+
+// ************************************************************************
+// * tCsvLineListByString class - The CSV Line will be sorted by a string
+// *    field
+// ************************************************************************
+
+type
+   tCsvLineListByString = class( tCsvLineList)
+      public
+         Key:    string;
+         constructor Create( iLine: tCsvStringArray);
+      end;
+
+
+// ************************************************************************
+// * tCsvLineListByInt64 class - The CSV Line will be sorted by an int64
+// *    field
+// ************************************************************************
+
+type
+   tCsvLineListByInt64 = class( tCsvLineList)
+      public
+         Key:    int64;
+         constructor Create( iLine: tCsvStringArray);
+      end;
+
+
+// ************************************************************************
+// * tCsvLineByIPv4 class - The CSV Line will be sorted by an IPv4
+// *                          address field
+// ************************************************************************
+
+type
+   tCsvLineListByIPv4 = class( tCsvLineList)
+      public
+         Key:    word32;
+         constructor Create( iLine: tCsvStringArray);
+      end;
 
 
 // ************************************************************************
@@ -28,17 +118,79 @@ type
 var
    FieldName:          string; // From the --header parameter
    FieldIndex:         integer;
-   SkipNonPrintable:   boolean;
-   IgnoreCase:         boolean;
-   ReverseOrder:       boolean;
+   SkipNonPrintable:   boolean = false;
+   IgnoreCase:         boolean = false;
+   ReverseOrder:       boolean = false;
    DelimiterIn:        char;
    DelimiterOut:       char;
    Csv:                tCsv;
-   SortByIpv4:         boolean;
-   SortByInteger:      boolean;
-   SortByDouble:       boolean;
-   StringDict:         tStringSortDictionary;
-   Int64Dict:          tInt64SortDictionary;
+   SortByIpv4:         boolean = false;
+   SortByInteger:      boolean = false;
+   SortByDouble:       boolean = false;
+
+
+
+// ========================================================================
+// = tCsvLineListByString class
+// ========================================================================
+// ************************************************************************
+// * Create() - Constructor
+// ************************************************************************
+
+constructor tCsvLineListByString.Create( iLine: tCsvStringArray);
+   var
+      L:  tCsvLine;
+   begin
+      inherited Create();
+      L:= tCsvLine.Create();
+      L.Row:= iLine;
+      Queue:= L;
+      if( IgnoreCase) then begin
+         Key:= LowerCase( iLine[ FieldIndex]);
+      end else begin
+         Key:= iLine[ FieldIndex];
+      end;
+   end; // Create()
+
+
+
+// ========================================================================
+// = tCsvLineListByInt64 class
+// ========================================================================
+// ************************************************************************
+// * Constructor()
+// ************************************************************************
+
+constructor tCsvLineListByInt64.Create( iLine: tCsvStringArray);
+   var
+      L:  tCsvLine;
+   begin
+      inherited Create();
+      L:= tCsvLine.Create();
+      L.Row:= iLine;
+      Queue:= L;
+      key:= StrToInt64( iLine[ FieldIndex]);
+   end; // Create()
+
+
+
+// ========================================================================
+// = tCsvLineListByIPv4 class
+// ========================================================================
+// ************************************************************************
+// * Constructor()
+// ************************************************************************
+
+constructor tCsvLineListByIPv4.Create( iLine: tCsvStringArray);
+   var
+      L:  tCsvLine;
+   begin
+      inherited Create();
+      L:= tCsvLine.Create();
+      L.Row:= iLine;
+      Queue:= L;
+      key:= StrToInt64( iLine[ FieldIndex]);
+   end; // Create()
 
 
 
@@ -49,7 +201,32 @@ var
 procedure SetGlobals();
    var
       Delimiter:    string;
+      Exclusive:    integer = 0;
    begin
+      // Set the boolean options
+      SkipNonPrintable:= ParamSet( 'skip-non-printable');
+      IgnoreCase:=       ParamSet( 'ignore-case');
+      ReverseOrder:=     ParamSet( 'reverse-order');
+      if( ParamSet( 'ipv4')) then begin
+         SortByIpv4:= true;
+         Inc( Exclusive);
+      end;
+      if( ParamSet( 'number')) then begin
+         SortByInteger:= true;
+         Inc( Exclusive);
+      end;
+      if( ParamSet( 'number')) then begin
+         SortByInteger:= true;
+         Inc( Exclusive);
+      end;
+      if( ParamSet( 'double')) then begin
+         SortByDouble:= true;
+         Inc( Exclusive);
+      end;
+      if( Exclusive > 1) then begin
+         raise tCsvException.Create( 'You selected more than one field sort type!');        
+      end;
+      
       // Set the input delimiter
       if( ParamSet( 'id')) then begin
          Delimiter:= GetParam( 'id');
@@ -90,20 +267,8 @@ procedure SetGlobals();
          Usage( true, 'Your header field ''' + FieldName + ''' does not exist in the input CSV file!');
       end;
 
-      // Set the boolean options
-      SkipNonPrintable:= ParamSet( 'skip-non-printable');
-      IgnoreCase:=       ParamSet( 'ignore-case');
-      ReverseOrder:=     ParamSet( 'reverse-order');
-      SortByIpv4:=       ParamSet( 'ipv4');
-      SortByInteger:=    ParamSet( 'number');
-      SortByDouble:=     ParamSet( 'double');
-
       // Apply the boolean options
       Csv.SkipNonPrintable:= SkipNonPrintable;
-
-      // Initialize the dictionaries;
-      StringDict:= tStringSortDictionary.Create( tStringSortDictionary.tCompareFunction(@CompareString), true);
-      Int64Dict:=  tInt64SortDictionary.Create( tInt64SortDictionary.tCompareFunction(@CompareInt64), true);
    end; // SetGlobals()
 
 
@@ -139,13 +304,66 @@ procedure InitArgvParser();
 
 
 // ************************************************************************
-// * CompareStrings() - compares two strings. Used by the string dictionary
+// * CompareByStrings() - Compare two Strings.  Used by tCsvTree
 // ************************************************************************
+
+function CompareString( CLL1: tCsvLineList; CLL2: tCsvLineList): int8;
+   var
+      K1: string;
+      K2: string;
+   begin
+      K1:= tCsvLineListByString( CLL1).Key;
+      K2:= tCsvLineListByString( CLL2).Key;
+      if( K1 > K2) then begin
+         result:= 1;
+      end else if( K1 < K2) then begin
+         result:= -1;
+      end else begin
+         result:= 0;
+      end;
+   end; // CompareString()
 
 
 // ************************************************************************
-// * CompareInt64() - Compare two Int64s.  Used by the Int64 dictionary
+// * CompareInt64() - Compare two Int64s.  Used by tCsvTree
 // ************************************************************************
+
+function CompareInt64( CLL1: tCsvLineList; CLL2: tCsvLineList): int8;
+   var
+      K1: Int64;
+      K2: Int64;
+   begin
+      K1:= tCsvLineListByInt64( CLL1).Key;
+      K2:= tCsvLineListByInt64( CLL2).Key;
+      if( K1 > K2) then begin
+         result:= 1;
+      end else if( K1 < K2) then begin
+         result:= -1;
+      end else begin
+         result:= 0;
+      end;
+   end; // CompareInt64()
+
+
+// ************************************************************************
+// * CompareWord32() - Compare two Word32s.  Used by tCsvTree
+// ************************************************************************
+
+function CompareWord32( CLL1: tCsvLineList; CLL2: tCsvLineList): int8;
+   var
+      K1: word32;
+      K2: word32;
+   begin
+      K1:= tCsvLineListByIPv4( CLL1).Key;
+      K2:= tCsvLineListByIpv4( CLL2).Key;
+      if( K1 > K2) then begin
+         result:= 1;
+      end else if( K1 < K2) then begin
+         result:= -1;
+      end else begin
+         result:= 0;
+      end;
+   end; // CompareInt64()
 
 
 // ************************************************************************
@@ -222,9 +440,5 @@ begin
    //    end;
    // until( C = EOFchr);
 
-   StringDict.RemoveAll( True);
-   StringDict.Destroy();
-   Int64Dict.RemoveAll( True);
-   Int64Dict.Destroy();
    Csv.Destroy;
 end.  // csv_sort program
