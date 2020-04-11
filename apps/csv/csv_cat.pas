5e36bf701a3b1a588cffaeab61959fd549728dc8
Lloyd B. Park <lloyd.b.park@gmail.com>
2020-04-10 23:36:53 -0400

    I added my copyright notice.  I had forgotten it as I worked on these CSV tools.

--- apps/csv/csv_cat.pas
+++ apps/csv/csv_cat.pas
@@ -1,3 +1,41 @@
+{* ***************************************************************************
+
+Copyright (c) 2019 by Lloyd B. Park
+
+Combines multiple .csv files with identical headers into one file.  Its cheif
+advantage over a simple text editor is that it check to make sure headers 
+match and only outputs the first one
+
+This file is part of Lloyd's Free Pascal Libraries (LFPL).
+
+    LFPL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as 
+    published by the Free Software Foundation, either version 2.1 of the 
+    License, or (at your option) any later version with the following 
+    modification:
+
+    As a special exception, the copyright holders of this library 
+    give you permission to link this library with independent modules
+    to produce an executable, regardless of the license terms of these
+    independent modules, and to copy and distribute the resulting 
+    executable under terms of your choice, provided that you also meet,
+    for each linked independent module, the terms and conditions of 
+    the license of that module. An independent module is a module which
+    is not derived from or based on this library. If you modify this
+    library, you may extend this exception to your version of the 
+    library, but you are not obligated to do so. If you do not wish to
+    do so, delete this exception statement from your version.
+
+    LFPL is distributed in the hope that it will be useful,but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General 
+    Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with LFPL.  If not, see <http://www.gnu.org/licenses/>.
+
+*************************************************************************** *}
+
 program csv_cat;
 
 {$include lbp_standard_modes.inc}
