40d2badfd759f087ec76b35b1a50411e795ec7d8
Lloyd Park <lloyd.b.park@gmail.com>
2020-12-19 22:57:59 -0500

    Making progress with the dns/dhcp config generator

--- apps/ipdb2/ipdb2_dns_dhcp_config_out.pas
+++ apps/ipdb2/ipdb2_dns_dhcp_config_out.pas
@@ -33,16 +33,28 @@ uses
    lbp_sql_db,  // SQL Exceptions
    ipdb2_home_config,
    ipdb2_tables,
+   ipdb2_flags,
    lbp_xdg_basedir,
    lbp_ip_utils,
+   lbp_generic_containers,
    sysutils;
 
+type
+   tSimpleNode = class
+      FullName: string;
+      IPString: string;
+      IPWord32: word32;
+   end;  // tSimpleNode Class
+
+type
+   tNodeDictionary = specialize tgDictionary< word64, tSimpleNode>;
 
 var
    FullNode:         FullNodeQuery;
    FullAlias:        FullAliasQuery;
    IPRanges:         IPRangesTable;
    Domains:          DomainsTable;
+   NodeDict:         tNodeDictionary;
    WorkingFolder:    string;  // The name of the working folder
    StaticFolder:     string;  // The name of the folder where static include data is stored.
    DhcpdConfWorking: string = 'dhcpd.conf';
@@ -51,15 +63,125 @@ var
    ProdDhcpFolder:   string = '/etc/dhcp/';
    DhcpdConf:        text;
    NamedConf:        text;
+   Zone:             text;
    DhcpMaxLeaseSecs: string = '3600'; // 1 hour
    DhcpDefLeaseSecs: string = '3600'; // 1 hour
+   DhcpDefDomain:    string = 'la-park.org';
 
 
 // ************************************************************************
-// * ProcessSubnet() - Process the passed IPRanges record
+// * LookupNode() - Returns the tSimpleNode matching the passed ID value.
+// ************************************************************************
+
+function LookupNode( ID: Word64): tSimpleNode;
+   var
+      IdStr:      string;
+      SimpleNode: tSimpleNode;
+   begin
+      result:= nil;
+      if( ID = 0) then exit;
+
+      // Try to get it from NodeDict
+      if NodeDict.Find( ID) then begin
+         result:= NodeDict.Value;
+      end else begin
+
+         // Try to look it up in FullNode and add it to NodeDict
+         FullNode.NodeID.SetValue( ID);
+         FullNode.Query( ' and NodeInfo.ID = ' + FullNode.NodeID.GetSqlValue);
+         if( FullNode.Next) then begin
+            SimpleNode:= tSimpleNode.Create();
+            SimpleNode.FullName:= FullNode.FullName;
+            SimpleNode.IPString:= FullNode.CurrentIP.GetValue;
+            SimpleNode.IPWord32:= FullNode.CurrentIP.OrigValue;
+
+            NodeDict.Add( FullNode.NodeID.OrigValue, SimpleNode);
+            result:= SimpleNode;
+         end; // If found in FullNode
+      end; // else try FullNode lookup
+   end; // LookupNode()
+
+
+// ************************************************************************
+// * ProcessDynamicRanges() - Output the DHCP Dynamic Ranges in this subnet
+// ************************************************************************
+
+procedure ProcessDynamicRanges();
+   begin
+     
+   end; // ProcessDynamicRanges()
+
+
+// ************************************************************************
+// * ProcessDhcpNode()
+// ************************************************************************
+
+procedure ProcessDhcpNode()
+   begin
+     
+   end; // ProcessDhcpNode()
+
+
+// ************************************************************************
+// * ProcessDNSPtr() Output the current FullNode record to the Zone file.
 // ************************************************************************
 
 
+// ************************************************************************
+// ProcessDhcpNetwork() - Output DHCP configuration for the current
+//                        IPRange
+// ************************************************************************
+
+procedure ProcessDhcpNetwork();
+   var
+      DNS:         string = '';
+      SimpleNode:  tSimpleNode;
+   begin
+      // Build the list of DNS servers
+      SimpleNode:= LookupNode( IPRanges.ClientDNS1.OrigValue);
+      if( SimpleNode = nil) then raise SQLdbException.Create( 'A Subnet doesn''t have DNS servers set!');
+      DNS:= SimpleNode.IPString;
+      SimpleNode:= LookupNode( IPRanges.ClientDNS2.OrigValue);
+      if( SimpleNode <> nil) then DNS:= DNS + ', ' + SimpleNode.IPString;
+      SimpleNode:= LookupNode( IPRanges.ClientDNS3.OrigValue);
+      if( SimpleNode <> nil) then DNS:= DNS + ', ' + SimpleNode.IPString;
+      DNS:= DNS + ';';
+      
+
+      writeln( DhcpdConf, 'subnet ', IpRanges.StartIP.GetValue(), ' netmask ',
+               IpRanges.NetMask.GetValue, ' {');
+      writeln( DhcpdConf, '   default-lease-time ', DhcpDefLeaseSecs);
+      writeln( DhcpdConf, '   max-lease-time ', DhcpMaxLeaseSecs);
+      writeln( DhcpdConf, '   option broadcast-address ', IpRanges.EndIp.GetValue);
+      writeln( DhcpdConf, '   option subnet-mask ', IpRanges.NetMask.GetValue);
+      writeln( DhcpdConf, '   option routers ', IpRanges.Gateway.GetValue);
+      writeln( DhcpdConf, '   option domain-name-servers ', DNS);
+      writeln( DhcpdConf, '   option domain-name ', DhcpDefDomain); 
+      writeln( DhcpdConf);  
+   end; // ProcessDhcpNetwork()
+
+
+// ************************************************************************
+// * ProcessSubnets() - Iterate through the subnets and process each one
+// ************************************************************************
+
+procedure ProcessSubnets();
+   var
+      Slash0Str:   string;
+      Slash32Str:  string;
+   begin
+      Str( Slash[  0], Slash0Str);
+      Str( Slash[ 32], Slash32Str);
+
+      // Query for all the subnets except those with an impossible netmask
+      IPRanges.Query( 'where NetMask != ' + Slash0Str + 
+                      ' and NetMask != ' + Slash32Str + 
+                      ' Order by NetMask, StartIP');
+      while( IPRanges.Next) do begin
+//         if( IPRanges.Flags.GetBit( OutputDNS)) then ProcessReverseZone();
+         if( IPRanges.Flags.GetBit( OutputDhcp)) then ProcessDhcpNetwork();
+      end; // For each range
+   end; // ProcessSubnets()
 
 
 // ************************************************************************
@@ -116,6 +238,8 @@ procedure Initialize();
       IPRanges:=   IPRangesTable.Create();
       Domains:=    DomainsTable.Create();
 
+      NodeDict:=   tNodeDictionary.Create( tNodeDictionary.tCompareFunction( @CompareWord64s));
+
       // Set our static and working folders
       StaticFolder:=  lbp_xdg_basedir.CacheFolder;
       StaticFolder:=  BuildFolder(  [ StaticFolder, 'lbp', 'ipdb2_dns_dhcp_config_out', 'static']);
@@ -147,6 +271,8 @@ procedure Finalize();
       Close( NamedConf);
       Close( DhcpdConf);
 
+      NodeDict.RemoveAll( True);
+      NodeDict.Destroy;
       Domains.Destroy;
       IPRanges.Destroy;
       FullAlias.Destroy;
@@ -160,7 +286,9 @@ procedure Finalize();
 
 begin
    Initialize();
+
+   ProcessSubnets();
  
-   IPRanges.Query( 'where NetMask >= ' + Slash[ 8])
+
    Finalize();
 end. // ipdb2_dns_dhcp_config_out program
